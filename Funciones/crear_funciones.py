0b247ebe981b872fcce8077049bcb271bcfb9d9c
AbrilGadea <asanchez923@mi.unc.edu.ar>
2023-05-09 17:25:48 -0300

    CURSO DE PYTHON

--- Funciones/crear_funciones.py
+++ Funciones/crear_funciones.py
@@ -18,7 +18,26 @@ def saludar(nombre, sexo):
     else:
         adjetivo = "crack"
     
-    print(f'Hola {nombre} mi {adjetivo}, ¿Cómo andas?')
+    #print(f'Hola {nombre} mi {adjetivo}, ¿Cómo andas?')
     
-saludar("Abril", "mujer")   
+#saludar("Abril", "mujer")   
  
+ #Crear una función que nos retorne valores
+def crear_contraseñas_random(num) :
+     chars = "abcdefghij"
+     num_entero = str(num)
+     num = int(num_entero[0])
+     c1 = num - 2
+     c2 = num
+     c3 = num -5
+     contraseña = f'{chars[c1]}{chars[c2]}{chars[c3]}{num_entero *2}'
+     return contraseña, num     
+     
+     #Desempaquetando la función
+     
+password, primer_numero = crear_contraseñas_random(981)
+
+#Mostrando los resultados obtenidos y los datos utilizados para obtenerlos
+print(f'Tu contraseña nueva es: {password}')
+print(f'El número utilizado para crearla fue: {primer_numero}')
+     
